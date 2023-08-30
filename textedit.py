3054c56409b8996179e5cd0558e067384d6f2a6e
Niklas Dieckow <niklas.dieckow@googlemail.com>
2023-08-31 00:15:30 +0200

    basic array writing in one line

--- textedit.py
+++ textedit.py
@@ -31,8 +31,8 @@ class Cursor:
 def move_handler(dir):
     move = getattr(Cursor, 'move_' + dir)
     move(cursor)
-    cursor_xpos = 5 + cursor.pos * 10
-    cursor_ypos = 5 + cursor.line * 15
+    cursor_xpos = 7 + cursor.pos * 8
+    cursor_ypos = 3 + cursor.line * 15
     canvas.coords(cursor_line, cursor_xpos, cursor_ypos, cursor_xpos, cursor_ypos + 15)
     canvas.update()
 
@@ -42,7 +42,22 @@ def up_handler(event): move_handler('up')
 def down_handler(event): move_handler('down')
 
 cursor = Cursor()
-#content = 
+content = []
+
+def key_handler(event):
+    global content, cursor
+    if len(event.char) == 0:
+        return
+    content.insert(cursor.pos, event.char)
+    move_handler('right')
+    update_text()
+
+def backspace_handler(event):
+    global content, cursor
+    #content.remove(cursor.pos - 1)
+    del content[cursor.pos - 1]
+    move_handler('left')
+    update_text()
 
 root = tk.Tk()
 root.geometry("850x450")
@@ -51,17 +66,22 @@ frm = tk.Frame(root, padx=5, pady=5)
 frm.grid()
 
 canvas = tk.Canvas(frm, width=800, height=400, bg="white")
+text_object = canvas.create_text(7, 2, text='', fill="black", anchor="nw", font=("Andale Mono",))
 
-canvas.create_text(7, 2, text="yo waddup", fill="black", anchor="nw", font="Monaco")
+def update_text():
+    canvas.itemconfigure(text_object, text=''.join(content))
+    canvas.update()
 
-cursor_xpos = 5 + cursor.pos * 10
-cursor_ypos = 5 + cursor.line * 15
-cursor_line = canvas.create_line(cursor_xpos, cursor_ypos, cursor_xpos, cursor_ypos + 15, fill="black", width=2)
+cursor_xpos = 7 + cursor.pos * 8
+cursor_ypos = 3 + cursor.line * 15
+cursor_line = canvas.create_line(cursor_xpos, cursor_ypos, cursor_xpos, cursor_ypos + 15, fill="black", width=1)
 
 canvas.bind('<Left>', left_handler)
 canvas.bind('<Right>', right_handler)
 canvas.bind('<Up>', up_handler)
 canvas.bind('<Down>', down_handler)
+canvas.bind('<Key>', key_handler)
+canvas.bind('<BackSpace>', backspace_handler)
 canvas.pack()
 canvas.focus_set()
 
